6d96942d84f1dc2144e95e6cf7fa18878fca91a6
Jebearssica <Jebearssica@outlook.com>
2021-03-20 22:12:45 +0800

    feat: 116.填充每个节点的下一个右侧节点指针

---
+++ Tree/116.填充每个节点的下一个右侧节点指针.cpp
@@ -0,0 +1,116 @@
+/*
+ * @lc app=leetcode.cn id=116 lang=cpp
+ *
+ * [116] 填充每个节点的下一个右侧节点指针
+ *
+ * https://leetcode-cn.com/problems/populating-next-right-pointers-in-each-node/description/
+ *
+ * algorithms
+ * Medium (69.08%)
+ * Likes:    423
+ * Dislikes: 0
+ * Total Accepted:    105.1K
+ * Total Submissions: 152.1K
+ * Testcase Example:  '[1,2,3,4,5,6,7]'
+ *
+ * 给定一个 完美二叉树 ，其所有叶子节点都在同一层，每个父节点都有两个子节点。二叉树定义如下：
+ * 
+ * 
+ * struct Node {
+ * ⁠ int val;
+ * ⁠ Node *left;
+ * ⁠ Node *right;
+ * ⁠ Node *next;
+ * }
+ * 
+ * 填充它的每个 next 指针，让这个指针指向其下一个右侧节点。如果找不到下一个右侧节点，则将 next 指针设置为 NULL。
+ * 
+ * 初始状态下，所有 next 指针都被设置为 NULL。
+ * 
+ * 
+ * 
+ * 进阶：
+ * 
+ * 
+ * 你只能使用常量级额外空间。
+ * 使用递归解题也符合要求，本题中递归程序占用的栈空间不算做额外的空间复杂度。
+ * 
+ * 
+ * 
+ * 
+ * 示例：
+ * 
+ * 
+ * 
+ * 
+ * 输入：root = [1,2,3,4,5,6,7]
+ * 输出：[1,#,2,3,#,4,5,6,7,#]
+ * 解释：给定二叉树如图 A 所示，你的函数应该填充它的每个 next 指针，以指向其下一个右侧节点，如图 B
+ * 所示。序列化的输出按层序遍历排列，同一层节点由 next 指针连接，'#' 标志着每一层的结束。
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 树中节点的数量少于 4096
+ * -1000 
+ * 
+ * 
+ */
+
+// @lc code=start
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+// class Node
+// {
+// public:
+//     int val;
+//     Node *left;
+//     Node *right;
+//     Node *next;
+
+//     Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+//     Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+//     Node(int _val, Node *_left, Node *_right, Node *_next)
+//         : val(_val), left(_left), right(_right), next(_next) {}
+// };
+class Solution
+{
+public:
+    void connect2Nodes(Node *left, Node *right)
+    {
+        if (left == nullptr || right == nullptr)
+            return;
+        left->next = right;
+        connect2Nodes(left->left, left->right);
+        connect2Nodes(right->left, right->right);
+        connect2Nodes(left->right, right->left);
+    }
+    Node *connect(Node *root)
+    {
+        if (root == nullptr)
+            return root;
+        connect2Nodes(root->left, root->right);
+        return root;
+    }
+};
+// @lc code=end
