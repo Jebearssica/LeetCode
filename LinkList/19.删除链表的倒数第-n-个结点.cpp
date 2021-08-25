84bc2d30a597e2f522945d3638cd3145ee716123
Jebearssica <Jebearssica@outlook.com>
2021-08-25 09:31:39 +0800

    19.删除链表的倒数第-n-个结点

---
+++ LinkList/19.删除链表的倒数第-n-个结点.cpp
@@ -0,0 +1,99 @@
+/*
+ * @lc app=leetcode.cn id=19 lang=cpp
+ *
+ * [19] 删除链表的倒数第 N 个结点
+ *
+ * https://leetcode-cn.com/problems/remove-nth-node-from-end-of-list/description/
+ *
+ * algorithms
+ * Medium (42.67%)
+ * Likes:    1519
+ * Dislikes: 0
+ * Total Accepted:    479.8K
+ * Total Submissions: 1.1M
+ * Testcase Example:  '[1,2,3,4,5]\n2'
+ *
+ * 给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。
+ * 
+ * 进阶：你能尝试使用一趟扫描实现吗？
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：head = [1,2,3,4,5], n = 2
+ * 输出：[1,2,3,5]
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：head = [1], n = 1
+ * 输出：[]
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：head = [1,2], n = 1
+ * 输出：[1]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 链表中结点的数目为 sz
+ * 1 
+ * 0 
+ * 1 
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+// struct ListNode
+// {
+//     int val;
+//     ListNode *next;
+//     ListNode() : val(0), next(nullptr) {}
+//     ListNode(int x) : val(x), next(nullptr) {}
+//     ListNode(int x, ListNode *next) : val(x), next(next) {}
+// };
+class Solution
+{
+public:
+    int idx = -1;
+    ListNode *traverse(ListNode *head, int n)
+    {
+        if (head == nullptr)
+        {
+            return nullptr;
+        }
+        ListNode *nextNode = traverse(head->next, n);
+        idx++;
+        if (n == idx)
+        {
+            head->next = nextNode->next;
+        }
+        return head;
+    }
+    ListNode *removeNthFromEnd(ListNode *head, int n)
+    {
+        ListNode *dummyNode = new ListNode(-1, head);
+        return traverse(dummyNode, n)->next;
+    }
+};
+// @lc code=end
