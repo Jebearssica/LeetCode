9bf3ec7dbdebe8987db6186a581b9ccff5ff6a76
Jebearssica <Jebearssica@outlook.com>
2021-10-09 15:44:41 +0800

    3.无重复字符的最长子串

---
+++ String/3.无重复字符的最长子串.cpp
@@ -0,0 +1,89 @@
+/*
+ * @lc app=leetcode.cn id=3 lang=cpp
+ *
+ * [3] 无重复字符的最长子串
+ *
+ * https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/description/
+ *
+ * algorithms
+ * Medium (38.00%)
+ * Likes:    6230
+ * Dislikes: 0
+ * Total Accepted:    1.3M
+ * Total Submissions: 3.3M
+ * Testcase Example:  '"abcabcbb"'
+ *
+ * 给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度。
+ * 
+ * 
+ * 
+ * 示例 1:
+ * 
+ * 
+ * 输入: s = "abcabcbb"
+ * 输出: 3 
+ * 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 
+ * 输入: s = "bbbbb"
+ * 输出: 1
+ * 解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+ * 
+ * 
+ * 示例 3:
+ * 
+ * 
+ * 输入: s = "pwwkew"
+ * 输出: 3
+ * 解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+ * 请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+ * 
+ * 
+ * 示例 4:
+ * 
+ * 
+ * 输入: s = ""
+ * 输出: 0
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 0 
+ * s 由英文字母、数字、符号和空格组成
+ * 
+ * 
+ */
+
+// @lc code=start
+#include <string>
+#include <unordered_map>
+using namespace std;
+class Solution
+{
+public:
+    int lengthOfLongestSubstring(string s)
+    {
+        unordered_map<char, int> window;
+        int left = 0, right = 0;
+        int res = 0;
+        while (right < s.size())
+        {
+            char chIn = s[right++];
+            window[chIn]++;
+            while (window[chIn] > 1)
+            {
+                char chOut = s[left++];
+                window[chOut]--;
+            }
+            res = res > (right - left) ? res : (right - left);
+        }
+        return res;
+    }
+};
+// @lc code=end
