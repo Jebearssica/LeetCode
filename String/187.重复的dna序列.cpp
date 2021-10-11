bb09c5f4c3b38a4f09e80e506b2377891cd77607
Jebearssica <Jebearssica@outlook.com>
2021-10-11 19:49:49 +0800

    187.重复的dna序列

---
+++ String/187.重复的dna序列.cpp
@@ -0,0 +1,70 @@
+/*
+ * @lc app=leetcode.cn id=187 lang=cpp
+ *
+ * [187] 重复的DNA序列
+ *
+ * https://leetcode-cn.com/problems/repeated-dna-sequences/description/
+ *
+ * algorithms
+ * Medium (48.40%)
+ * Likes:    235
+ * Dislikes: 0
+ * Total Accepted:    52.7K
+ * Total Submissions: 105.2K
+ * Testcase Example:  '"AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT"'
+ *
+ * 所有 DNA 都由一系列缩写为 'A'，'C'，'G' 和 'T' 的核苷酸组成，例如："ACGAATTCCG"。在研究 DNA 时，识别 DNA
+ * 中的重复序列有时会对研究非常有帮助。
+ * 
+ * 编写一个函数来找出所有目标子串，目标子串的长度为 10，且在 DNA 字符串 s 中出现次数超过一次。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：s = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT"
+ * 输出：["AAAAACCCCC","CCCCCAAAAA"]
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：s = "AAAAAAAAAAAAA"
+ * 输出：["AAAAAAAAAA"]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 0 
+ * s[i] 为 'A'、'C'、'G' 或 'T'
+ * 
+ * 
+ */
+
+// @lc code=start
+#include <vector>
+#include <string>
+#include <unordered_map>
+using namespace std;
+class Solution
+{
+public:
+    vector<string> findRepeatedDnaSequences(string s)
+    {
+        vector<string> res;
+        unordered_map<string, int> map;
+        int length = s.size();
+        for (int idx = 0; idx <= length - 10; idx++)
+        {
+            string substr = s.substr(idx, 10);
+            if (++map[substr] == 2)
+                res.push_back(substr);
+        }
+        return res;
+    }
+};
+// @lc code=end
