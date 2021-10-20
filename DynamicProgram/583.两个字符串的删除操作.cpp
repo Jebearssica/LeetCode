07458086d4fcefe002f1e2696e3e7371aa71fa37
Jebearssica <Jebearssica@outlook.com>
2021-10-20 18:44:49 +0800

    update advanced code

--- DynamicProgram/583.两个字符串的删除操作.cpp
+++ DynamicProgram/583.两个字符串的删除操作.cpp
@@ -45,28 +45,23 @@ public:
     int minDistance(string word1, string word2)
     {
         int length1 = word1.size(), length2 = word2.size();
-        vector<vector<int>> dp(length1 + 1, vector<int>(length2 + 1, 0));
-        //init
+        vector<int> dp(length2 + 1, 0);
         for (int i = 1; i <= length1; i++)
         {
-            dp[i][0] = i;
-        }
-        for (int j = 1; j <= length2; j++)
-        {
-            dp[0][j] = j;
-        }
-
-        for (int i = 1; i <= length1; i++)
-        {
+            auto ch1 = word1[i - 1];
+            int upL = 0;
             for (int j = 1; j <= length2; j++)
             {
-                if (word1[i - 1] == word2[j - 1])
-                    dp[i][j] = dp[i - 1][j - 1];
+                int upR = dp[j];
+                auto ch2 = word2[j - 1];
+                if (ch1 == ch2)
+                    dp[j] = upL + 1;
                 else
-                    dp[i][j] = (dp[i - 1][j] < dp[i][j - 1] ? dp[i - 1][j] : dp[i][j - 1]) + 1;
+                    dp[j] = max(dp[j - 1], upR);
+                upL = upR;
             }
         }
-        return dp[length1][length2];
+        return length1 + length2 - 2 * dp[length2];
     }
 };
 // @lc code=end
