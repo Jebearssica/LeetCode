b81b1177a2b1941c48cb5eba3581550c9a492856
Jebearssica <Jebearssica@outlook.com>
2021-11-17 20:34:03 +0800

    37.解数独 二刷

--- BackTracking/37.解数独.cpp
+++ BackTracking/37.解数独.cpp
@@ -6,113 +6,102 @@
  * https://leetcode-cn.com/problems/sudoku-solver/description/
  *
  * algorithms
- * Hard (66.59%)
- * Likes:    660
+ * Hard (67.04%)
+ * Likes:    932
  * Dislikes: 0
- * Total Accepted:    58.8K
- * Total Submissions: 88.3K
+ * Total Accepted:    97.3K
+ * Total Submissions: 145K
  * Testcase Example:  '[["5","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]]'
  *
  * 编写一个程序，通过填充空格来解决数独问题。
  * 
- * 一个数独的解法需遵循如下规则：
+ * 数独的解法需 遵循如下规则：
  * 
  * 
  * 数字 1-9 在每一行只能出现一次。
  * 数字 1-9 在每一列只能出现一次。
- * 数字 1-9 在每一个以粗实线分隔的 3x3 宫内只能出现一次。
+ * 数字 1-9 在每一个以粗实线分隔的 3x3 宫内只能出现一次。（请参考示例图）
  * 
  * 
- * 空白格用 '.' 表示。
- * 
- * 
- * 
- * 一个数独。
+ * 数独部分空格内已填入了数字，空白格用 '.' 表示。
  * 
  * 
  * 
- * 答案被标成红色。
+ * 
+ * 
+ * 
+ * 示例：
+ * 
+ * 
+ * 输入：board =
+ * [["5","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]]
+ * 
+ * 输出：[["5","3","4","6","7","8","9","1","2"],["6","7","2","1","9","5","3","4","8"],["1","9","8","3","4","2","5","6","7"],["8","5","9","7","6","1","4","2","3"],["4","2","6","8","5","3","7","9","1"],["7","1","3","9","2","4","8","5","6"],["9","6","1","5","3","7","2","8","4"],["2","8","7","4","1","9","6","3","5"],["3","4","5","2","8","6","1","7","9"]]
+ * 解释：输入的数独如上图所示，唯一有效的解决方案如下所示：
+ * 
+ * 
+ * 
+ * 
+ * 
  * 
  * 提示：
  * 
  * 
- * 给定的数独序列只包含数字 1-9 和字符 '.' 。
- * 你可以假设给定的数独只有唯一解。
- * 给定数独永远是 9x9 形式的。
+ * board.length == 9
+ * board[i].length == 9
+ * board[i][j] 是一位数字或者 '.'
+ * 题目数据 保证 输入数独仅有一个解
+ * 
+ * 
+ * 
  * 
  * 
  */
 
 // @lc code=start
 #include <vector>
+using namespace std;
 class Solution
 {
 public:
-    /*
-    m19: matrix 1*9
-    m91: matrix 9*1
-    m33: matrix 3*3
-    */
-    int m19[9];
-    int m91[9];
-    int m33[3][3];
-    bool valid;
-    //将一对值合成为一个值, 储存空格的表
-    std::vector<std::pair<int, int>> spaces;
-
-    void insertNumber(int i, int j, int number)
+    vector<pair<int, int>> space;
+    bool isValid(vector<vector<char>> &board, int row, int col, char digit)
     {
-        m19[i] ^= (1 << number);
-        m91[j] ^= (1 << number);
-        m33[i / 3][j / 3] ^= (1 << number);
-    }
-
-    void dfs(std::vector<std::vector<char>> &board, int pos)
-    {
-        //遍历完了所有空格, 则成功
-        if (pos == spaces.size())
-        {
-            valid = true;
-            return;
-        }
-        //提出空格所在位置
-        auto [i, j] = spaces[pos];
-        //注意1~9一共有9位>8, 所以一共有两个字节0000 000'9' 8~1, 记得9的高位还有7个0
-        //必须要全为0的地方才能叫做合法位置
-        int ValidPos = ~(m19[i] | m91[j] | m33[i / 3][j / 3]) & 0x1ff;
-        for (; ValidPos && !valid; ValidPos &= (ValidPos - 1))
-        {
-            //用的msvc compiler 找到最低位为1的后面的零的个数, 就能知道对应的数字是多少
-            //leetcode 用的gcc...
-            int number = __builtin_ctz(ValidPos & (-ValidPos));
-            insertNumber(i, j, number);
-            board[i][j] = number + '0' + 1;
-            dfs(board, pos + 1);
-            insertNumber(i, j, number);
-        }
-    }
-
-    void solveSudoku(std::vector<std::vector<char>> &board)
-    {
-        //初始化
-        memset(m19, 0, sizeof(m19));
-        memset(m91, 0, sizeof(m91));
-        memset(m33, 0, sizeof(m33));
-        valid = false;
-
-        //读取数据
-        //列
+        //check the row and col
         for (int i = 0; i < 9; i++)
-            //行
-            for (int j = 0; j < 9; j++)
-                //空的
-                //有数字的, 存入对应位置
-                if (board[i][j] == '.')
-                    spaces.emplace_back(i, j);
-                else
-                    insertNumber(i, j, int(board[i][j] - '0' - 1));
-        //整个数独初始化完毕, 开始解
-        dfs(board, 0);
+            if (board[i][col] == digit || board[row][i] == digit)
+                return false;
+        //check the 3x3 matrix
+        for (int i = row / 3 * 3; i < row / 3 * 3 + 3; i++)
+            for (int j = col / 3 * 3; j < col / 3 * 3 + 3; j++)
+                if (board[i][j] == digit)
+                    return false;
+        return true;
+    }
+    bool backtracing(vector<vector<char>> &board, int pos)
+    {
+        if (pos == space.size())
+            return true;
+        auto [row, col] = space[pos];
+        for (char digit = '1'; digit <= '9'; digit++)
+        {
+            if (!isValid(board, row, col, digit))
+                continue;
+            board[row][col] = digit;
+            if (backtracing(board, pos + 1))
+                return true;
+            board[row][col] = '.';
+        }
+        return false;
+    }
+    void solveSudoku(vector<vector<char>> &board)
+    {
+        //find the empty space
+        for (int row = 0; row < 9; row++)
+            for (int col = 0; col < 9; col++)
+                if (board[row][col] == '.')
+                    space.emplace_back(row, col);
+        backtracing(board, 0);
     }
 };
 // @lc code=end
