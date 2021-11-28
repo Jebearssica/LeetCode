32886e59302a3f77ef43bb6b5d02bc2741db7e5d
Jebearssica <Jebearssica@outlook.com>
2021-11-28 16:39:53 +0800

    200.岛屿数量

---
+++ BackTracking/200.岛屿数量.cpp
@@ -0,0 +1,100 @@
+/*
+ * @lc app=leetcode.cn id=200 lang=cpp
+ *
+ * [200] 岛屿数量
+ *
+ * https://leetcode-cn.com/problems/number-of-islands/description/
+ *
+ * algorithms
+ * Medium (56.11%)
+ * Likes:    1426
+ * Dislikes: 0
+ * Total Accepted:    357.2K
+ * Total Submissions: 636.2K
+ * Testcase Example:  '[["1","1","1","1","0"],["1","1","0","1","0"],["1","1","0","0","0"],["0","0","0","0","0"]]'
+ *
+ * 给你一个由 '1'（陆地）和 '0'（水）组成的的二维网格，请你计算网格中岛屿的数量。
+ * 
+ * 岛屿总是被水包围，并且每座岛屿只能由水平方向和/或竖直方向上相邻的陆地连接形成。
+ * 
+ * 此外，你可以假设该网格的四条边均被水包围。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：grid = [
+ * ⁠ ["1","1","1","1","0"],
+ * ⁠ ["1","1","0","1","0"],
+ * ⁠ ["1","1","0","0","0"],
+ * ⁠ ["0","0","0","0","0"]
+ * ]
+ * 输出：1
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：grid = [
+ * ⁠ ["1","1","0","0","0"],
+ * ⁠ ["1","1","0","0","0"],
+ * ⁠ ["0","0","1","0","0"],
+ * ⁠ ["0","0","0","1","1"]
+ * ]
+ * 输出：3
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * m == grid.length
+ * n == grid[i].length
+ * 1 
+ * grid[i][j] 的值为 '0' 或 '1'
+ * 
+ * 
+ */
+
+// @lc code=start
+#include <vector>
+using namespace std;
+class Solution
+{
+public:
+    void dfs(vector<vector<char>> &grid, int row, int col)
+    {
+        //check the validation
+        if (row < 0 || col < 0 || row >= grid.size() || col >= grid[0].size())
+            return;
+        //check whether island or sea
+        if (grid[row][col] == '0')
+            return;
+        //exchange the island to sea for saving the visited matrix
+        grid[row][col] = '0';
+        //dfs
+        dfs(grid, row + 1, col);
+        dfs(grid, row - 1, col);
+        dfs(grid, row, col + 1);
+        dfs(grid, row, col - 1);
+    }
+    int numIslands(vector<vector<char>> &grid)
+    {
+        int res = 0;
+        for (int row = 0; row < grid.size(); row++)
+        {
+            for (int col = 0; col < grid[0].size(); col++)
+            {
+                if (grid[row][col] == '1')
+                {
+                    res++;
+                    dfs(grid, row, col);
+                }
+            }
+        }
+        return res;
+    }
+};
+// @lc code=end
